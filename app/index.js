73bb4ac9709d3933f33b50ad4737c136a98a71d0
ginniehench <ginniehench@gmail.com>
2017-08-14 15:55:27 -0400

    answered leap prompt. go back to others.

--- app/index.js
+++ app/index.js
@@ -7,6 +7,19 @@ const chalk = require('chalk');
 
 var now = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
 var nth_day_of_year = moment().format("DDDo");
+// var seconds_into_the_day = moment().format("DDDo");
+// var daylightSavings =
+var leapYear = moment().isLeapYear();
+var leapYearIs = "";
+
+function leapYearTrueFalse() {
+  if (leapYear === true){
+    leapYearIs = "is"
+  }else{
+    leapYearIs = "is not"
+  }
+  return leapYearIs
+}
 
 
 // var b = moment([2017, 1, 1]);
@@ -20,5 +33,7 @@ var nth_day_of_year = moment().format("DDDo");
 // var dayWrapper = moment(day);
 
 
-console.log(`It is: ${now}.`)
-console.log(`It is the: ${nth_day_of_year} day of the year.`);
+console.log(`It is ${now}.`)
+console.log(`It is the ${nth_day_of_year} day of the year.`);
+// console.log(`It is ${} seconds into the day.`);
+console.log(`It ${leapYearTrueFalse(leapYear)} a leap year.`)
